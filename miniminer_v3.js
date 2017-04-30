13497cf0238743cae04c1cd8a1e3f4cc85bb266e
Killfrra <Killfrra@users.noreply.github.com>
2017-04-30 17:56:23 +0300

    Added comments and webworker

--- miniminer_v3.js
+++ miniminer_v3.js
@@ -1,3 +1,5 @@
+/* jshint bitwise : false */
+/* jshint esversion : 6 */
 var debug = true;
 var Utils = {
 	stringToInts: function (input) {
@@ -35,8 +37,7 @@ var Utils = {
 	}
 };
 
-//var _hasher = new Hash();
-var Data ,target/*, Current*/, _ticks,tmp;
+var Data ,target, _ticks = 0,share;
 
 var _averageSpeed = 0; //hs
 var login = "1BhKdW7omrkQrWa3VtS8PwtDuMDCK2uRu1:ANYTHING@localhost:80/getwork.php";
@@ -50,49 +51,81 @@ var _batchSize = 1000000;
 var xhr = new XMLHttpRequest();
 var retryTime = 30000;
 var _lastPrint = Date.now();
+var _nonce = 0;
+var workid = 0;
+var sending_share = false;
+var getting_work = false;
 
-/*function Sha256(input) {
-	_hasher.update(input);
-	var digest = _hasher.digest();
-	_hasher.clean();
-	return digest;
-}*/
-
-function Main(args) {
-	var hash, _nonce = 0;
-	while ((Date.now() - _ticks) <= _maxAge && _nonce < 4294967295) {
-		for (let batchSize = _batchSize; batchSize > 0; batchSize--) {
-			hash = sha256d(_nonce);
-			//count trailing bytes that are zero
-			for (let i = 7; i >= 0; i--) {
-				if ((hash[i] >>> 0) > (target[i] >>> 0))break;
-				if ((hash[i] >>> 0) < (target[i] >>> 0)){ //share difficulty matched!
-					console.log("*** Found Valid Share ***");
-					if (debug) {
-						console.log("Share: " + Utils.intsToString(Data) + Utils.intToString(_nonce));
-						console.log("Nonce: " + Utils.intToString(_nonce));
-						console.log("Hash: " + Utils.intsToString(hash));
-					}
-					SendShare(_nonce);
-					return;
+var cmd = {
+	new_work : 0,
+	send_share : 1,
+	finished : 2,
+	update : 3
+};
+var cmd2str = ["new_work","send_share","finished","update"];
+var threads = new Array(1);
+var hpt = 4294967296 / threads.length;//hashes per thread
+GetWork(function(){ //всё начинается здесь
+	for(let i = 0;i < threads.length; i++){
+		threads[i] = new Worker('worker.js');
+		threads[i].onmessage = function(arg){	//по возвращении worker доложится о цели своего прибытия
+			console.log(cmd2str[arg.data[0]],arg.data.slice(1));
+			if(arg.data[0] == cmd.send_share){	//если worker хочет отправить share
+				this.idle = true;	//он простаивает до тех пор,пока не придёт ответ с pool'а. Экономия ресурсов в надежде на положительный ответ
+				if(sending_share === false){//если никто ещё не озадачен этим
+					//то что будет отправлено. Помещается в переменную,что бы не генерировать снова в случае сбоя отправки
+					share = Utils.AddPadding(Utils.EndianFlip32BitChunks(Utils.intsToString(Data)+Utils.intToString(_nonce)));
+					SendShare(arg.data[1]);
+					sending_share = true;	//говорит остальным простаивать по возращении
 				}
+			}else if(arg.data[0] == cmd.finished || (Date.now() - _ticks) >= _maxAge){//если worker проверил всё,что ему было велено,и ничего не нашёл (о чём и сообщил) или не успел
+				if(debug)console.log("Work outdated.Passed "+(Date.now() - _ticks)+" ms");
+				this.idle = true;	//он простаивает
+				if(getting_work === false){//если никто ещё не озадачен этим
+					GetWork(update_pending);//до тех пор,пока не прийдёт запрошенная в этой строке новая работа
+					getting_work = true;	//все простаивают
+				}
+			}else if(arg.data[0] == cmd.update){	//если worker спрашивает : "нет ли более новой работы?"
+				this.idle = false;	//раз срок действия работы не истёк (раз мы сюда дошли),скучать ему не придётся
+				if(this.workid != workid)this.update_work();	//если работа сменилась за время отсутствия,то так и сообщаем
+				else this.postMessage([-1]);			//иначе пусть работать продолжает
+				//console.log("Nonce: " + Utils.intToString(arg.data[1]) + "/ffffffff " + ((_nonce / 4294967295) * 100).toFixed(2) + "%");
+				var speed = Math.floor((_batchSize * 1000) / (Date.now() - _lastPrint)); /*=_batchSize div ((now - _lastPrint) div 1000)*/
+				_averageSpeed = (_averageSpeed + speed) >> 1; //div 2
+				console.log("Speed of worker number "+i+" = " + (speed / 1000) + " Kh/s");
+				_lastPrint = Date.now();
 			}
-			//increase
-			if (++_nonce == 4294967295) break;
-		}
-		console.log("Data: " + Utils.intsToString(Data));
-		console.log("Nonce: " + Utils.intToString(_nonce) + "/ffffffff " + ((_nonce / 4294967295) * 100).toFixed(2) + "%");
-		console.log("Hash: " + Utils.intsToString(hash));
-		var speed = Math.floor((_batchSize * 1000) / (Date.now() - _lastPrint)); /*=_batchSize div ((now - _lastPrint) div 1000)*/
-		_averageSpeed = (_averageSpeed + speed) >> 1; //div 2
-		console.log("Speed: " + (speed / 1000) + " Kh/s");
-		_lastPrint = Date.now();
+		};
+		(threads[i].update_work = function(){
+			console.log(cmd.new_work,hpt*i,(hpt*(i+1))-1,Data[16],Data[17],Data[18],midstate,target);
+			threads[i].postMessage([
+				cmd.new_work,
+				hpt*i,
+				(hpt*(i+1))-1,
+				Data[16],
+				Data[17],
+				Data[18],
+				midstate,
+				target
+			]);
+			threads[i].idle = false;
+			threads[i].workid = workid;
+		})();
 	}
-	if(debug)console.log("Work outdated.Passed "+(Date.now() - _ticks)+" ms");
-	GetWork();
+});
+
+function update_pending(){
+	GetWork(function(){
+		for(let i = 0; i < threads.length; i++)if(threads[i].idle)threads[i].update_work();
+	});
 }
 
-function GetWork() {
+function terminate(){threads[0].terminate();clearTimeout(getwork_timeout);}
+
+var getwork_timeout,prev_data = "";
+
+function GetWork(callback) {
+	clearTimeout(getwork_timeout);
 	console.log("Requesting Work from Pool...");
 	if (debug) {
 		console.log("Server URL: " + Url);
@@ -100,13 +133,20 @@ function GetWork() {
 		console.log("Password: " + Password);
 	}
 	InvokeMethod("getwork", function () {
-		var data = JSON.parse(xhr.response);
-		Data = Utils.stringToInts(Utils.EndianFlip32BitChunks(Utils.RemovePadding(data.data))).slice(0,20);
-		sha256d_init(Data);
-		target = Utils.stringToInts(Utils.EndianFlip32BitChunks(data.target));
-		//Current = new Uint8Array(Data);
+		data = JSON.parse(xhr.response);
+		if(data.data != prev_data){
+			console.log(data.data);
+			console.log(prev_data);
+			Data = Utils.stringToInts(Utils.EndianFlip32BitChunks(Utils.RemovePadding(data.data))).slice(0,20);
+			sha256d_init(Data);
+			target = Utils.stringToInts(Utils.EndianFlip32BitChunks(data.target));
+			workid++;
+		}
 		_ticks = Date.now();
-		Main();
+		getting_work = false;
+		if(callback !== undefined)callback();
+		getwork_timeout = setTimeout(GetWork,_maxAge);
+		prev_data = data.data;
 	}, GetWork);
 }
 
@@ -135,12 +175,17 @@ function InvokeMethod(method, onSuccess, onError, paramString) {
 	xhr.send("{\"id\": 0, \"method\": \"" + method + "\", \"params\": [" + ((paramString !== void 0) ? ("\"" + paramString + "\"") : "") + "]}");
 }
 
-function SendShare(_nonce) {
+function SendShare(_nonce,onerror) {
 	console.log("Sending Share to Pool...");
 	InvokeMethod("getwork", function () {
-		if (JSON.parse(xhr.response).result === true)
+		if (JSON.parse(xhr.response).result === true){
 			console.log("Server accepted the Share!");
-		else console.error("Server declined the Share!");
-		GetWork();
-	}, SendShare, Utils.AddPadding(Utils.EndianFlip32BitChunks(Utils.intsToString(/*Current*/Data)+Utils.intToString(_nonce))));
+			sending_share = false;
+			update_pending();
+		}else{
+			console.error("Server declined the Share!");
+			onerror();//шукаємо далі
+		}
+		
+	}, SendShare, share);
 }
